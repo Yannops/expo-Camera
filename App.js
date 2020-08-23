0eee0ad6b547b514d767ab6461f442faad2dc834
Yannops <yannsilveira34@gmail.com>
2020-08-23 20:44:26 -0300

    Salvando a foto tirada na galeria do celular

--- App.js
+++ App.js
@@ -1,70 +1,86 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal, Image, PermissionsAndroid, CameraRoll } from 'react-native';
-import {Camera} from 'expo-camera';
-import {FontAwesome} from '@expo/vector-icons';
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const camRef = useRef(null);
   const [capturedPhoto, setCapturedPhoto] = useState(null);
   const [open, setOpen] = useState(false);
+  const [haspermission, setHaspermission] = useState(null);
 
-async function takePicture(){
-  if(camRef){
-    const data = await camRef.current.takePictureAsync();
-    setCapturedPhoto(data.uri);
-    setOpen(true);  
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHaspermission(status === 'granted');
+    })();
+
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      setHaspermission(status === 'granted');
+    })();
+  }, []);
+
+  if (haspermission === null) {
+    return <View />
   }
-}
 
-/*submitPicture = async () => {
-  try {
-    const granted = await PermissionRequest.request(
-      PermissionRequest.PERMISSIONS.READ_EXTERNAL_STORAGE,
-      {
-        "title": "Access Storage",
-        "message": "Access Storage for the pictures"
-      }
-    )
-    if (granted === Permissions.RESULTS.GRANTED) {
-      await CameraRoll.saveToCameraRoll(capturedPhoto);
-    } else {
-      console.log("Permissão Negada!");
+  if (haspermission === false) {
+    return <Text>Acesso Negado!</Text>
+  }
+
+  async function takePicture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true);
     }
-  } catch (error) {
-    console.warn(error)
   }
-  setCapturedPhoto(null);
-}*/
+
+  async function savePicture() {
+    const asset = await MediaLibrary.createAssetAsync(capturedPhoto)
+      .then(() => {
+        setTimeout(() => {
+          alert('Foto Salvada na Galeria!');
+        }, 1000);
+        setOpen(false);
+      }).catch(error => {
+        console.log('err: ', error);
+      });
+  }
 
 
 
   return (
-      <Camera style={{flex: 1}} type={type} ref={camRef}>
-        <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row'}}>
-          <TouchableOpacity style={{position: "absolute", bottom: 20, left: 20}} onPress={()=>{
-            setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
-          }}>
-            <FontAwesome name="backward" color="blue" size={25}></FontAwesome>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={takePicture}>
-          <FontAwesome name="camera" size={40} color="white"></FontAwesome>
-        </TouchableOpacity>      
-        {capturedPhoto && 
+    <Camera style={{ flex: 1 }} type={type} ref={camRef}>
+      <Text style={{ marginTop: 70, textAlign: 'center', fontSize: 20, fontWeight: 'bold', color: '#fff' }}>App de Fotos</Text>
+      <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+        <TouchableOpacity style={{ position: "absolute", bottom: 20, left: 20 }} onPress={() => {
+          setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+        }}>
+          <MaterialCommunityIcons name="camera-switch" color="#fff" size={35} />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={takePicture}>
+        <FontAwesome name="camera" size={40} color="white"></FontAwesome>
+      </TouchableOpacity>
+      {capturedPhoto &&
         <Modal animationType="slide" transparent={false} visible={open}>
-          <View style={{flex: 1, justifyContent: "center", alignItems: "center", margin: 20}}>
-            <Image style={{width: '100%', height: 300, borderRadius: 20}} source={{uri: capturedPhoto}}></Image>
-            <TouchableOpacity style={{margin: 10, position: "absolute", left: 40, top: 510}} onPress={()=> setOpen(false)}>
+          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Image style={{ width: '100%', height: '100%', borderRadius: 20 }} source={{ uri: capturedPhoto }}></Image>
+            <TouchableOpacity style={{ position: 'absolute', left: 70, top: 750 }} onPress={savePicture}>
               <FontAwesome name="check" size={50} color="green"></FontAwesome>
             </TouchableOpacity>
-            <TouchableOpacity style={{margin: 10, position: "absolute", left: 220, top: 510}} onPress={()=> setOpen(false)}>
+            <TouchableOpacity style={{ position: 'absolute', left: 300, top: 750 }} onPress={() => setOpen(false)}>
               <FontAwesome name="close" size={50} color="red"></FontAwesome>
             </TouchableOpacity>
           </View>
-          </Modal>
-        }
-        </Camera>
+        </Modal>
+      }
+    </Camera>
   );
 }
 
@@ -78,6 +94,6 @@ const styles = StyleSheet.create({
   button: {
     justifyContent: 'center',
     alignItems: 'center',
+    marginBottom: 60,
   },
-
 });
