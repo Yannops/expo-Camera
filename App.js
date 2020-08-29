11826557f0f486ec949ca3c362fea7e5f6bc37c1
Yannops <yannsilveira34@gmail.com>
2020-08-29 00:56:48 -0300

    Video implementado

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Modal, Image } from 'react-native';
 import { Camera } from 'expo-camera';
-import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Video } from 'expo-av';
+import { FontAwesome, MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
-  const [type, setType] = useState(Camera.Constants.Type.back);
   const camRef = useRef(null);
-  const [capturedPhoto, setCapturedPhoto] = useState(null);
   const [open, setOpen] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flashOn, setFlashOn] = useState(Camera.Constants.FlashMode.off);
+  const [isPreview, setIsPreview] = useState(false);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
+  const [videoSource, setVideoSource] = useState(null);
+  const [isVideoRecording, setIsVideoRecording] = useState(null);
   const [haspermission, setHaspermission] = useState(null);
 
   useEffect(() => {
@@ -19,7 +24,7 @@ export default function App() {
     })();
 
     (async () => {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
       setHaspermission(status === 'granted');
     })();
   }, []);
@@ -40,6 +45,35 @@ export default function App() {
     }
   }
 
+  async function recordVideo() {
+    if (camRef.current) {
+      try {
+        const videoRecordPromisse = camRef.current.recordAsync();
+
+        if (videoRecordPromisse) {
+          setIsVideoRecording(true);
+          const data = await videoRecordPromisse;
+          const source = data.uri;
+          if (source) {
+            setIsPreview(true);
+            setVideoSource(source);
+          }
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+  };
+
+  async function stopRecording() {
+    if (camRef.current) {
+      setIsPreview(false);
+      setIsVideoRecording(false);
+      camRef.current.stopRecording();
+      setOpen(true);
+    }
+  };
+
   async function savePicture() {
     const asset = await MediaLibrary.createAssetAsync(capturedPhoto)
       .then(() => {
@@ -52,35 +86,78 @@ export default function App() {
       });
   }
 
+  async function saveVideo() {
+    const asset = await MediaLibrary.createAssetAsync(videoSource)
+      .then(() => {
+        setTimeout(() => {
+          alert('Video Salvado na Galeria!');
+        }, 1000);
+        setOpen(false);
+      }).catch(error => {
+        console.log('err: ', error);
+      });
+  }
 
+  function deleteImage() {
+    setCapturedPhoto(null);
+    setOpen(false);  
+  }
+
+  function deleteVideo() {
+    setVideoSource(null);
+    setOpen(false);
+  }
+
+ console.log(Camera.Constants.FlashMode.on);
 
   return (
-    <Camera style={{ flex: 1 }} type={type} ref={camRef}>
-      <Text style={{ marginTop: 70, textAlign: 'center', fontSize: 20, fontWeight: 'bold', color: '#fff' }}>App de Fotos</Text>
-      <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
-        <TouchableOpacity style={{ position: "absolute", bottom: 20, left: 20 }} onPress={() => {
-          setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
-        }}>
-          <MaterialCommunityIcons name="camera-switch" color="#fff" size={35} />
+    <>
+      <Camera style={{ flex: 1 }} type={type} ref={camRef} >
+        <Text style={{ marginTop: 70, textAlign: 'center', fontSize: 20, fontWeight: 'bold', color: '#fff' }}>App de Fotos</Text>
+        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+          <TouchableOpacity style={{ position: "absolute", bottom: 20, left: 20 }} onPress={() => {
+            setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+          }}>
+            <MaterialCommunityIcons name="camera-switch" color="#fff" size={35} />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ position: "absolute", bottom: 100, left: 20 }}  onPress={() => {
+            setFlashOn(flashOn === Camera.Constants.FlashMode.on ? console.log('show') : console.log('fudeu'));
+          }}>
+            <Entypo name="flash" color="#fff" size={35} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button} onPressOut={stopRecording} onLongPress={recordVideo} onPress={takePicture}>
+          <FontAwesome name="camera" size={40} color="white"></FontAwesome>
         </TouchableOpacity>
-      </View>
-      <TouchableOpacity style={styles.button} onPress={takePicture}>
-        <FontAwesome name="camera" size={40} color="white"></FontAwesome>
-      </TouchableOpacity>
-      {capturedPhoto &&
-        <Modal animationType="slide" transparent={false} visible={open}>
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Image style={{ width: '100%', height: '100%', borderRadius: 20 }} source={{ uri: capturedPhoto }}></Image>
-            <TouchableOpacity style={{ position: 'absolute', left: 70, top: 750 }} onPress={savePicture}>
-              <FontAwesome name="check" size={50} color="green"></FontAwesome>
-            </TouchableOpacity>
-            <TouchableOpacity style={{ position: 'absolute', left: 300, top: 750 }} onPress={() => setOpen(false)}>
-              <FontAwesome name="close" size={50} color="red"></FontAwesome>
-            </TouchableOpacity>
-          </View>
-        </Modal>
-      }
-    </Camera>
+        {capturedPhoto &&
+          <Modal animationType="slide" transparent={false} visible={open}>
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+              <Image style={{ width: '100%', height: '100%', borderRadius: 20 }} source={{ uri: capturedPhoto }}></Image>
+              <TouchableOpacity style={{ position: 'absolute', left: 70, top: 750 }} onPress={savePicture}>
+                <FontAwesome name="check" size={50} color="green"></FontAwesome>
+              </TouchableOpacity>
+              <TouchableOpacity style={{ position: 'absolute', left: 300, top: 750 }} onPress={() => deleteImage()}>
+                <FontAwesome name="close" size={50} color="red"></FontAwesome>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+        }
+      </Camera>
+      <>
+        {videoSource &&
+          <Modal animationType="slide" transparent={false} visible={open}>
+            <Video shouldPlay={true} style={{ width: 500, height: 900, borderRadius: 20 }} source={{ uri: videoSource }}>
+              <TouchableOpacity style={{ position: 'absolute', left: 70, top: 750, zIndex: 1 }} onPress={saveVideo}>
+                <FontAwesome name="check" size={50} color="green"></FontAwesome>
+              </TouchableOpacity>
+              <TouchableOpacity style={{ position: 'absolute', left: 300, top: 750, zIndex: 1 }} onPress={() => deleteVideo()}>
+                <FontAwesome name="close" size={50} color="red"></FontAwesome>
+              </TouchableOpacity>
+            </Video>
+          </Modal>
+        }
+      </>
+    </>
   );
 }
 
